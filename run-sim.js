711dee009a8f82f1b524846b232b516a982d7b24
bayowulf <jvgallagheriv@gmail.com>
2026-09-17 15:37:40 -0400

    v0.6.3: drop simulated OutOfBounds runtime errors; inject Bug cards randomly into the middle of the program

--- run-sim.js
+++ run-sim.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const ROOT = __dirname;
 const HTML = path.join(ROOT, "simulator.html");
-const VERSION = "0.6.2";
+const VERSION = "0.6.3";
 
 function makeEl(){
   const el = {
